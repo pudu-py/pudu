cfb49ed09c581295001171acea29478f4a826e2c
Enric Grau <patenric@gmail.com>
2023-01-13 17:08:33 +0100

    coverage and new pudu

--- tests/test_pudu.py
+++ tests/test_pudu.py
@@ -2,17 +2,48 @@
 
 """Tests for `pudu` package."""
 
-import unittest
+import matplotlib.pyplot as plt
+import spectrapepper as spep
+# from pudu import pudu
 import pudu as pudu
+import numpy as np
+import unittest
+import pickle
 
 class TestPudu(unittest.TestCase):
     """Tests for `pudu` package."""
 
     def setUp(self):
-        """Set up test fixtures, if any."""
+        pass
 
     def tearDown(self):
-        """Tear down test fixtures, if any."""
+        pass
 
-    def test_000_something(self):
-        """Test something."""
+    def test(self):
+        x = spep.load('../examples/data/features.txt')[0]
+        x = x[np.newaxis, np.newaxis, :, np.newaxis]
+        y = spep.load('../examples/data/targets.txt', transpose=True)[2][0]
+
+        lda = pickle.load(open('../examples/data/lda_model.sav', 'rb'))
+        pca = pickle.load(open('../examples/data/pca_model.sav', 'rb'))
+
+        results = spep.load('pudu_test_results.txt')
+
+        def pf(X):
+            X = X[0,:,:,0]
+            return lda.predict_proba(pca.transform(X))[0]
+
+        imp = pudu.pudu(x, y, pf)
+
+        imp.importance(delta=1, window=100, calc='absolute')
+        imp.speed(delta=1, window=100, calc='absolute')
+        imp.synergy(delta=1, inspect=3, window=100, calc='absolute')
+        
+        self.assertEqual((imp.imp[0,0,:,0] == results[0]).all(), True)
+        self.assertEqual((imp.imp_norm[0,0,:,0] == results[1]).all(), True)
+
+        self.assertEqual((imp.spe[0,0,:,0] == results[2]).all(), True)
+        self.assertEqual((imp.spe_norm[0,0,:,0] == results[3]).all(), True)
+
+        self.assertEqual((imp.syn[0,0,:,0] == results[4]).all(), True)
+        self.assertEqual((imp.syn_norm[0,0,:,0] == results[5]).all(), True)
